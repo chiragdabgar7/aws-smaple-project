9f7abc8c3ec4827edabac0d25c39cc00ed14a846
Chirag Dabgar <chirag.dabgar@cimpress.com>
2020-10-15 18:13:00 +0530

    lambda function creation

---
+++ lambda_func.py
@@ -0,0 +1,20 @@
+import boto3
+import json
+
+#client = boto3.client('lambda')
+
+def lambda_build(LambdaFunctionName, iamRole):
+    client = boto3.client('lambda')
+
+    create_lambda_function = client.create_function(
+        FunctionName=LambdaFunctionName,
+        Runtime='python3.7',
+        Role=iamRole,
+        Handler='{}.lambda_build'.format('lambda_func'),
+        Description='Start a virtual machine',
+	    Code = {'S3Bucket':'test-bucket-chirag20201510', 'S3Key':'ssm.zip'}
+        )
+    return create_lambda_function
+
+
+print(lambda_build('My_func2','arn:aws:iam::368633732841:role/lambda-test-role'))
