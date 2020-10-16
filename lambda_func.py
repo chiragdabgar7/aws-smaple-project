8287b253a7d3e544ca65ae926d306f6363fedbec
chiragdabgar7 <chiragdabgar7@gmail.com>
2020-10-16 12:02:34 +0530

    making lambda function changes

--- lambda_func.py
+++ lambda_func.py
@@ -10,11 +10,12 @@ def lambda_build(LambdaFunctionName, iamRole):
         FunctionName=LambdaFunctionName,
         Runtime='python3.7',
         Role=iamRole,
-        Handler='{}.lambda_build'.format('lambda_func'),
+        Handler='ssm.lambda_handler',
         Description='Start a virtual machine',
-	    Code = {'S3Bucket':'test-bucket-chirag20201510', 'S3Key':'ssm.zip'}
+	    Code = {'S3Bucket':'test-bucket-20201610', 'S3Key':'ssm.zip'},
+        Timeout=10
         )
     return create_lambda_function
 
 
-print(lambda_build('My_func2','arn:aws:iam::368633732841:role/lambda-test-role'))
+print(lambda_build('My_func','arn:aws:iam::368633732841:role/lambda-test-role'))
